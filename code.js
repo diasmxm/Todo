35f661fd676a5d6589cbd1291f88e00e0ac0eace
diasmxm <stronghold996@mail.ru>
2021-10-22 22:03:30 +0300

    js+webpack

--- code.js
+++ code.js
@@ -18,19 +18,80 @@ function createElement(tag, content, className, parent, position){
     parent.insertAdjacentElement(position, el);
     return el;
 }
-const input = createElement('input', '', 'input', root, 'afterbegin');
-const btn = createElement('button', 'add', 'btn', root, 'beforeend');
-const ul = createElement('ul', '', 'ul', root, 'beforeend');
-const todo = new Array();
+const main_columns = createElement('div', '', 'columns', root, 'afterbegin');
+main_columns.style.paddingTop = '3em';
+const column = createElement('div', '', 'column', main_columns, 'afterbegin');
+column.classList.add('is-half');
+column.classList.add('is-offset-one-quarter');
+const input = createElement('input', '', 'input', column, 'afterbegin');
+const column2 = createElement('div', '', 'column', main_columns, 'beforeend');
+const btn = createElement('button', 'add', 'button', column2, 'beforeend');
+const toggle = createElement('input', '', 'toggle', column2, 'beforeEnd');
+toggle.setAttribute('type', 'checkbox')
+btn.classList.add('is-primary');
 
+const ol = createElement('ol', '', 'ol', column, 'beforeend'); // обертка списка
+ol.style.margin = '1em 0 0 2em';
+ol.setAttribute('type', 'A');
+const todo = [];
+let delete_btns = [];
 
 btn.onclick = () => {
-    // console.log(input.value);
-    todo.push(input.value);
-    ul.innerHTML = '';
-    input.value = '';
-    todo.map((item)=>{
-        createElement('li', item, 'li', ul, 'beforeend');
-    });
+    addTodo();
 };
+
+input.onkeydown = (e) => {
+    if(e.code == 'Enter') {
+        addTodo();
+    }
+};
+
+let flag = false;
+toggle.onchange = () =>{
+    if(!flag){
+        document.body.style.backgroundColor = 'black';
+        flag = true
+    }else{
+        document.body.style.backgroundColor = 'white';
+        flag = false;
+    }
+}
+
+
+function addTodo() {
+    if(input.value == '') {
+        return;
+    }
+    todo.push(input.value);
+    ol.innerHTML = ''; // тут обнуляем, мы каждый раз перерисовываем элементы
+    input.value = '';
+    todo.map((item, index) => {
+        let li = createElement('li', item, 'li', ol, 'beforeend');
+        li.style.marginTop = '1em';
+        li.setAttribute('key', index)
+        let btn_delete = createElement('button', 'delete', 'button', li, 'beforeend');
+        btn_delete.classList.add('is-danger');
+        btn_delete.classList.add('is-small');
+        btn_delete.style.marginLeft = '3em';
+        delete_btns = [];
+        delete_btns.push(btn_delete);
+        delete_btns.map((btn, index)=>{
+            btn.onclick = () =>{
+                const key = btn.parentNode.getAttribute('key');
+                todo.splice(key, 1);
+                btn.parentNode.remove()
+            }
+        })
+    });
+}
+
+
+// оформить интерфейс
+// input уменьшить
+// посередине
+// кнопку справа
+// li добавить border
+// добавление дела на нажатие Enter
+// удаление li (добавить для этого кнопку л li)
+// редактирование дела
 
